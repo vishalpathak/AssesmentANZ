c3680be02a405b2cf6b58d7f096f6c4cf4fa7727
vishal <vishal.p@borqs.com>
2020-03-17 18:32:09 +0530

    Initial Commit

---
+++ Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assessment
+//
+//  Created by Borqs India on 17/03/20.
+//  Copyright © 2020 Vishal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
